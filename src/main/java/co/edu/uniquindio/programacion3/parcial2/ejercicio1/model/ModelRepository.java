7ef0f703d0fb2dfac4f29dd3ee37f72005dac345
chaos086 <luisa.marinh@uqvirtual.edu.co>
2024-10-25 00:38:28 -0500

    Se modifica el model

--- src/main/java/co/edu/uniquindio/programacion3/parcial2/ejercicio1/model/ModelRepository.java
+++ src/main/java/co/edu/uniquindio/programacion3/parcial2/ejercicio1/model/ModelRepository.java
@@ -12,16 +12,29 @@ import lombok.ToString;
 @ToString
 public class ModelRepository implements Serializable {
 
+    private ArrayList<Departamento> listaDepartamentos;
+    private ArrayList<Proyecto> listaProyectos;
     private ArrayList<Empleado> listaEmpleados;
 
     public ModelRepository() {
         this.listaEmpleados = new ArrayList<>();
+        this.listaDepartamentos = new ArrayList<>();
+        this.listaProyectos = new ArrayList<>();
     }
+
 
 
     // agregar elementos -------------
     public void addEmpleado(Empleado usuario) {
         this.listaEmpleados.add(usuario);
+    }
+
+    public void addProyecto(Proyecto proyecto) {
+        this.listaProyectos.add(proyecto);
+    }
+
+    public void addDepartamento(Departamento departamento) {
+        this.listaDepartamentos.add(departamento);
     }
 
 
@@ -31,6 +44,20 @@ public class ModelRepository implements Serializable {
     }
     public void removeEmpleado(Empleado usuario) {
         this.listaEmpleados.remove(usuario);
+    }
+
+    public void removeProyecto(int index) {
+        this.listaProyectos.remove(index);
+    }
+    public void removeProyecto(Proyecto proyecto) {
+        this.listaProyectos.remove(proyecto);
+    }
+
+    public void removeDepartamento(int index) {
+        this.listaDepartamentos.remove(index);
+    }
+    public void removeDepartamento(Departamento departamento) {
+        this.listaDepartamentos.remove(departamento);
     }
 
 }
