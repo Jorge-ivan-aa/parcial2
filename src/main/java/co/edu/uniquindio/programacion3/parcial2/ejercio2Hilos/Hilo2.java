789a5bc12ee31d0c49b969e4b82d9164fbb775b2
chaos086 <luisa.marinh@uqvirtual.edu.co>
2024-10-24 23:05:25 -0500

    Se crea en funcionamiento completo el ejercicio2 de Hilos.

---
+++ src/main/java/co/edu/uniquindio/programacion3/parcial2/ejercio2Hilos/Hilo2.java
@@ -0,0 +1,73 @@
+package co.edu.uniquindio.programacion3.parcial2.ejercio2Hilos;
+
+public class Hilo2 extends Thread {
+    boolean runHilo2;
+    Double promedioMatriz;
+
+
+    public Double getPromedioMatriz(){
+        return promedioMatriz;
+    }
+    public Hilo2(){
+        this.runHilo2 = true;
+    }
+
+    int[][] m = {
+
+            {60 ,22, 41, 5, 2},
+            {13, 33, 44, 5, 3},
+            {89, 10, 100, 99, 1},
+
+    };
+    int veces=calcularTamaño(m);
+    @Override
+    public void run(){
+        System.out.println("Hilo 2 iniciado.");
+        promedioMatriz = Double.valueOf(recorrerMatriz(m)/veces);
+
+        int contador =0;
+        while (runHilo2){
+
+            try {
+
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if(contador ==veces)
+                runHilo2 = false;
+            contador++;
+        }
+        System.out.println("Promedio de la matriz= "+promedioMatriz);
+        System.out.println("Hilo 2 terminado.");
+
+    }
+
+    public int recorrerMatriz(int [][]m){
+
+        int suma =0;
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                suma+=m[i][j];
+
+            }
+
+        }
+
+        return suma;
+
+    }
+
+    public int calcularTamaño(int[][] m) {
+        if (m == null || m.length == 0) {
+            return 0;
+        }
+
+        int filas = m.length;
+        int columnas = m[0].length;
+
+        return filas * columnas;
+    }
+
+}
---
+++ src/main/java/co/edu/uniquindio/programacion3/parcial2/ejercio2Hilos/MainEjecutar.java
@@ -0,0 +1,31 @@
+package co.edu.uniquindio.programacion3.parcial2.ejercio2Hilos;
+
+public class MainEjecutar {
+    public static void main (String []args) throws InterruptedException {
+
+        //creación de instancia de hilo t1 y t2
+        Hilo1 t1 = new Hilo1();
+        Hilo2 t2 = new Hilo2();
+
+        //Inicialización de Hilos
+        t1.start();
+        t2.start();
+
+        //Terminación de hilos
+        t1.join();
+        t2.join();
+
+        //S3
+        int a=t1.getNumeroMenor();
+        Double b=t2.getPromedioMatriz();
+        Double c;
+        if(b!=0){
+            c=a/b;
+            System.out.println("Resultado de C= "+ c);
+        }else{
+            System.out.println("Resultado no valido");
+        }
+        System.out.println("Fin del programa :)");
+    }
+
+}
